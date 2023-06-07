0db131a7f3633492959b8943c0ce7c0000ad9869
Clayvets <anamaria271127@gmail.com>
2023-06-06 20:52:09 -0500

    Varias cosas varias xd

--- Assets/Scripts/IKSnapper.cs
+++ Assets/Scripts/IKSnapper.cs
@@ -7,10 +7,13 @@ public class IKSnapper : MonoBehaviour
     [SerializeField][Range(0,1)] private float proceduralInfluence;
     [SerializeField] private MultiParentConstraint[] animatedBones;
     [SerializeField] private MultiParentConstraint[] proceduralBones;
-    
+    //[SerializeField] private AnimationCurve activationAnimation;
+    //[SerializeField] private AnimationCurve deactivationAnimation;
+    [SerializeField] private float intepolationSpeed = 1.0f;
 
+    private float weightt;
+   
     private bool overrideTrigger;
-
     private void UpdateInfluence(float weight)
     {
         if (animatedBones == null) return;
@@ -30,19 +33,27 @@ public class IKSnapper : MonoBehaviour
         }
     }
 
+    public void SetWeight(bool state)
+    {
+        Debug.Log(state);
+        weightt += Time.deltaTime * intepolationSpeed * (state ? 1 : -1);
+        weightt = Mathf.Clamp01(weightt);
+        //UpdateInfluence();
+    }
+
     private void LateUpdate()
     {
-        if (overrideTrigger)
+        /*if (overrideTrigger)
         {
             proceduralInfluence = Mathf.Lerp(proceduralInfluence, proceduralInfluence + 3, Time.deltaTime);
         }
         else
         {
             proceduralInfluence = Mathf.Lerp(proceduralInfluence, proceduralInfluence - 3, Time.deltaTime);
-        }
+        }*/
 
         proceduralInfluence = Mathf.Clamp01(proceduralInfluence);
-        UpdateInfluence(proceduralInfluence);
+        UpdateInfluence(weightt);
     }
 
     private void OnValidate()
